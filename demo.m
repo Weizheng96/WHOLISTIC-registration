131379a41dd5d9487564d20b0afcd2f20b1ad151
Weizheng96 <56321962+Weizheng96@users.noreply.github.com>
2025-04-07 15:03:50 -0400

    Update demo.m

--- demo.m
+++ demo.m
@@ -5,8 +5,7 @@ addpath(genpath("./src"));
 
 %% set parameters
 % data path used for registration
-filePath="/work/public/Virginia Rutten/221124_f338_ubi_gCaMP7f_bactin_mCherry_CAAX_8505_7dpf_hypoxia/" + ...
-    "exp0/imag/221124_f338_ubi_gCaMP7f_bactin_mCherry_CAAX_7dpf002.nd2";
+filePath="./data/demo_data.mat";
 
 % number of downsampled pyramid layers.
 option.layer=3;
@@ -27,26 +26,25 @@ thresFactor=5; % set thresFactor=inf if all pixel should be considered
 maskRange=[5 500]; % set maskRange=[0 inf] if all pixel should be considered
 
 %% load template image and moving imagre
-reader = bfGetReader(convertStringsToChars(filePath));
-dat_ref=readOneFrame_single(reader,1,2);  %1st frame of 2nd channel as template image
-dat_mov=readOneFrame_single(reader,48,2); %48th frame of 2nd channel as moving image
-
+load(filePath,"dat_ref","dat_mov","zRatio");
+dat_ref=single(dat_ref);
+dat_mov=single(dat_mov);
 %% [optional] get unreliable region mask for moving immuning cells
 option.mask_ref=getMask(dat_ref,thresFactor);
 option.mask_mov=getMask(dat_mov,thresFactor);
 option.mask_ref=bwareafilt3_Wei(option.mask_ref,maskRange);
 option.mask_mov=bwareafilt3_Wei(option.mask_mov,maskRange);
-
+option.zRatio=zRatio;
 %% initialization
-[X,Y,Z,T,~,option.zRatio]=readMeta(reader);
+[X,Y,Z]=size(dat_ref);
 xG=option.r+1:2*option.r+1:X;yG=option.r+1:2*option.r+1:Y;zG=1:Z; % get control point
 option.motion=zeros([X,Y,Z,3]); % initial motion field
 smoothPenalty=getSmPnltNormFctr(dat_ref,option)*smoothPenalty_raw; % smoothness penalty
 
 %% registration (main function)
 disp("correct motion...");
-motion_current=getMotionHZR_Wei_v2d2(dat_mov,dat_ref,smoothPenalty,option); % get motion field
-dat_cor=correctMotion_Wei_v2(dat_mov,motion_current); %% apply the motion field to get corrected image
+motion_field=getMotionHZR_Wei_v2d2(dat_mov,dat_ref,smoothPenalty,option); % get motion field
+dat_cor=correctMotion_Wei_v2(dat_mov,motion_field); %% apply the motion field to get corrected image
 
 %% visulize result of 7th z slice
 vis_ref=(dat_ref(:,:,7)-100)/300;
